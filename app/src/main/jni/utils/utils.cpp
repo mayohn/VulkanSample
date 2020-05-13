77644e675620d0eb24b900d5dd86f51c4608afa6
mayongheng <92314428hnl>
2020-05-14 07:53:36 +0800

    提交
    Signed-off-by: mayongheng <92314428hnl>

---
+++ app/src/main/jni/utils/utils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mayongheng on 2020/5/13.
+//
+
+#include "utils.h"
---
+++ app/src/main/jni/utils/utils.h
@@ -0,0 +1,8 @@
+//
+// Created by mayongheng on 2020/5/13.
+//
+
+#ifndef VULKANSAMPLE_UTILS_H
+#define VULKANSAMPLE_UTILS_H
+
+#endif //VULKANSAMPLE_UTILS_H
